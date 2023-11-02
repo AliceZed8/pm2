b8b8102dde786dcbb4b3af308852f0f33c14abce
Alice Zed <38034672+AliceZed8@users.noreply.github.com>
2023-11-02 22:24:37 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,200 +1,141 @@
 #include <iostream>
 #include <stack>
-#include <sstream>
+#include <vector>
 
-bool __isDigit(char c) {
-	return (c >= '0') && (c <= '9');
-}
+namespace SomeCalc {
 
-bool __isOperator(char c) {
-	return ((c == '+') || (c == '-') || (c == '*') || (c == '/'));
-}
-
-int __Priority(char c) {
-	switch (c)
-	{
-	case '(':
-	case ')':
-		return 1;
-
-	case '+':
-	case '-':
-		return 2;
-
-	case '*':
-	case '/':
-		return 3;
-
+	bool check_brackets(const std::string& str) {
+		size_t count = 0;
+		for (char c : str) {
+			if (c == '(') { count++; continue; }
+			if (c == ')') count--;
+		}
+		return count == 0 ? true : false;
 	}
-}
 
-bool check_brackets(const std::string& str) {
-	std::stack<char> brackets;
-	for (char c : str) {
-		if (c == '(' || c == '[' || c == '{') {
-			brackets.push(c);
-			continue;
-		}
-
-		if (c == ')' || c == ']' || c == '}') {
-
-			if (brackets.empty()) return false;
-			if (c == ']' && brackets.top() != '[') return false;
-			if (c == ')' && brackets.top() != '(') return false;
-			if (c == '}' && brackets.top() != '{') return false;
-			brackets.pop();
-		}
-
+	bool is_digit(char c) {
+		return (c >= '0') && (c <= '9');
 	}
-	if (brackets.empty()) return true;
-	else return false;
-}
 
-double cpp_eval(std::string& input) {
-	std::stack<char> st;
-	std::string output;
-	std::string number;
+	bool is_operator(char c) {
+		return c == '+' || c == '-' || c == '*' || c == '/' || c == '~';
+	}
 
-	if (!check_brackets(input)) throw std::exception("Скобки расставлены неправильно");
+	int op_priority(char c) {
+		switch (c)
+		{
+		case '(':
+		case ')': return 0;
 
+		case '+':
+		case '-': return 1;
 
+		case '*':
+		case '/': return 2;
 
-	bool NUM_READ = false;
-	for (char &c : input) {
-		//Reading Number
-		if (__isDigit(c)) {
-			if (!NUM_READ) NUM_READ = true;
-			number.push_back(c);
-			continue;
+		case '~': return 3;
 		}
+	}
 
-		//Other symbol or operator
-		if ((c == ' ') || (c == '(') || (c == ')') || __isOperator(c)) {
+	void proccess_operation(std::stack<double>& st, char op) {
+		if (st.empty()) throw std::exception("eval:proccess_operation: недостаточно операндов. ошибка при чтении данных");
 
-			//Stop reading a number
-			if (NUM_READ) {
-				NUM_READ = false;
-				number.push_back(' ');
-				output += number;
-				number.clear();
-			}
+		if (op == '~') {
+			double right = st.top(); st.pop();
+			st.push(-right);
+			return;
+		}
+		double right = st.top(); st.pop();
+		double left = st.top(); st.pop();
+		switch (op) {
+		case '+':
+			st.push(left + right); break;
+		case '-':
+			st.push(left - right); break;
+		case '*':
+			st.push(left * right); break;
+		case '/':
+			if (right == 0) throw std::exception("eval:proccess_operation: деление на ноль");
+			st.push(left / right);
+			break;
+		}
+	}
 
-			if (__isOperator(c)) {
-				if (st.empty() || (__Priority(st.top()) < __Priority(c))) {
-					st.push(c);
-					continue;
-				}
 
-				while (!st.empty() && (__Priority(st.top()) >= __Priority(c))) {
-					if (st.top() == '+') output += "+ ";
-					if (st.top() == '-') output += "- ";
-					if (st.top() == '*') output += "* ";
-					if (st.top() == '/') output += "/ ";
-					st.pop();
-				}
+	double eval(std::string& input) {
+		bool NUM_READ = false;
+		std::stack<double> nums;
+		std::stack<char> ops;
+		std::string operand;
 
-				if (st.empty() || (__Priority(st.top()) < __Priority(c))) {
-					st.push(c);
-					continue;
-				}
+		if (!check_brackets(input)) throw std::exception("eval: cкобки расставлены неверно");
 
-			}
 
-			if (c == '(') {
-				st.push(c);
+
+		for (char& c : input) {
+			if (is_digit(c)) {
+				if (!NUM_READ) NUM_READ = true;
+				operand.push_back(c);
 				continue;
 			}
 
-			if (c == ')') {
-				while (st.top() != '(') {
-					if (st.top() == '+') output += "+ ";
-					if (st.top() == '-') output += "- ";
-					if (st.top() == '*') output += "* ";
-					if (st.top() == '/') output += "/ ";
-					st.pop();
-				}
-
-				st.pop();
-			}
-		}
-		if (c == '=') {
 			if (NUM_READ) {
 				NUM_READ = false;
-				number.push_back(' ');
-				output += number;
-				number.clear();
+				nums.push(atoi(operand.c_str()));
+				operand.clear();
 			}
-			break;
-		}
 
-	}
-	while (!st.empty()) {
-		if (st.top() == '+') output += "+ ";
-		if (st.top() == '-') output += "- ";
-		if (st.top() == '*') output += "* ";
-		if (st.top() == '/') output += "/ ";
-		st.pop();
-	}
+			if (c == '(') ops.push('(');
+			else if (c == ')') {
+				if (ops.top() == '(') throw std::exception("eval: Пустые скобки");
 
-	std::cout << "ОПЗ: " << output << std::endl;
-	std::stringstream stream(output);
-	std::stack<double> nums;
-	double num1, num2;
-
-	std::string buf;
-
-	while (!stream.eof()) {
-		stream >> buf;
-
-		if (__isDigit(buf[0])) {
-			nums.push(atof(buf.c_str()));
-			buf.clear();
-			continue;
-		}
-
-		if (__isOperator(buf[0])) {
-			if (nums.size() < 2) throw std::exception("Ошибка 1");
-
-			num1 = nums.top();
-			nums.pop();
-			num2 = nums.top();
-			nums.pop();
-
-			if (buf[0] == '+') nums.push(num1 + num2);
-			if (buf[0] == '-') nums.push(num2 - num1);
-			if (buf[0] == '*') nums.push(num2 * num1);
-			if (buf[0] == '/') {
-				if (num1 == 0) throw std::exception("Деление на ноль!!!");
-				nums.push(num2 / num1);
+				while (ops.top() != '(')
+				{
+					proccess_operation(nums, ops.top());
+					ops.pop();
+				}
+				ops.pop();
 			}
-			buf.clear();
+
+			if (is_operator(c)) {
+				while (!ops.empty() && (op_priority(ops.top()) >= op_priority(c))) {
+					proccess_operation(nums, ops.top());
+					ops.pop();
+				}
+				ops.push(c);
+			}
 		}
+		if (NUM_READ) {
+			NUM_READ = false;
+			nums.push(atoi(operand.c_str()));
+			operand.clear();
+		}
+
+		while (!ops.empty()) {
+			proccess_operation(nums, ops.top());
+			ops.pop();
+		}
+
+		return nums.top();
 	}
-
-	if (nums.size() != 1) throw std::exception("Ошибка 1");
-
-	return nums.top();
-
 }
-
-
 int main() {
 	setlocale(LC_ALL, "RUS");
 	std::string ex;
 	double res;
 	while (true)
 	{
-		std::cout << "Введите выражение: " ;
+		std::cout << "Введите выражение: ";
 		std::getline(std::cin, ex);
 
 		if (ex == "exit") return 0;
 		try
 		{
-			res = cpp_eval(ex);
+			res = SomeCalc::eval(ex);
 		}
 		catch (const std::exception& e)
 		{
-			std::cout << e.what() << std::endl;
+			std::cout << "error: " << e.what() << std::endl;
 			continue;
 		}
 
